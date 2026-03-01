35f5995c35f91ff5b46dc111fea1d52fee55603b
KhaledSger <khaled.sger@gmail.com>
2026-03-01 15:18:50 +0200

    fix: resolve ruff linting violations

--- main.tf
+++ main.tf
@@ -21,6 +21,31 @@ resource "random_pet" "bucket_name" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
+  # checkov:skip=CKV_AWS_18:Logging not required for this demo
+  # checkov:skip=CKV_AWS_144:Cross-region replication not required for this demo
   bucket = random_pet.bucket_name.id
-  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.example_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
+  bucket = aws_s3_bucket.example_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.example_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
